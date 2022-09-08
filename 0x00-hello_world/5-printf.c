021bab582bcb3a68d08cfcc70d3070cd5c213ea8
Mtheop <motheophetla@gmail.com>
2022-09-08 13:18:29 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>                                                                                                                     
+                                                                                                                                        
+/**                                                                                                                                    
+* main - print the string in the put function                                                                                           
+*                                                                                                                                       
+* Description: using the main functions                                                                                                 
+* this program prints "with proper grammer, but the outcome is a piece of art,"                                                         
+* Return: 0                                                                                                                             
+*/                                                                                                                                      
+int main(void)                                                                                                                          
+{                                                                                                                                       
+
+        printf("with proper grammer, but the outcome is a piece of art,\n");                                                            
+
+        return (0);                                                                                                                     
+}                              
