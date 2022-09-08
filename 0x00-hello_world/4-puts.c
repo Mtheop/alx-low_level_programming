643179a353e7b95be70032315e9e52f4b80bb53a
Mtheop <motheophetla@gmail.com>
2022-09-08 11:54:09 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#Include <stdio.h>
+
+/**
+*Print the main with put function
+*
+*Use the main function
+*Program prints "Programming is like building a multilingual puzzle"
+* Returen: 0
+*/
+Int main(void)
+{
+        puts(*\"Programming is like building a multilingual puzzle");
+        return (0);
+}
