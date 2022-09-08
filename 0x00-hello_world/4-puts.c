8ade0921743fb897cfbc1344a0550b24dd9e7960
Mtheop <motheophetla@gmail.com>
2022-09-08 12:17:48 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-*mainn-print the main with put function
+* mainn-print the main with put function
 *
-*Description: Use the main function
-*program prints "Programming is like building a multilingual puzzle"
+* Description: Use the main function
+* program prints "Programming is like building a multilingual puzzle"
 * Returen: 0
 */
 int main(void)
