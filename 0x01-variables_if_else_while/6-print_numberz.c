9ad0d7287b50c4345d0da39ee04c86cd6f5a06be
Mtheop <motheophetla@gmail.com>
2022-09-09 12:53:51 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main point of program entry
+ *
+ * Description: print all single digit numbers
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar (i);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
