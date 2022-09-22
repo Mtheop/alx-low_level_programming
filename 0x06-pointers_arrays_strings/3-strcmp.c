bc8ca71a9253513edfa96e6db7606433e0f15975
Mtheop <motheophetla@gmail.com>
2022-09-22 10:44:01 -0700

    returns

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,10 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: zero if s1 == s2
- * negative number if s1 < s2
- * positive number if s1 > s2
+ * Return:
+ * returns zero if s1 == s2
+ * returns negative number if s1 < s2
+ * returns positive number if s1 > s2
  */
 
 int _strcmp(char *s1, char *s2)
@@ -19,17 +20,20 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] ==
 	'\0' && s2[i] == '\0')
 			break;
+
 		else if (s1[i]
 			== '\0')
 		{
 			diff == s2[i];
 			break;
 		}
+
 		else if (s1[i] != s2[i])
 		{
 			diff = s1[i] - s2[i];
 			break;
 		}
+
 		else
 			i++;
 	}
