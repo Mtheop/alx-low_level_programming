01f94bcab58319610d3757090bdd4812d29815e6
Mtheop <motheophetla@gmail.com>
2022-10-12 09:55:22 -0700

    Hot fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,3 @@
-#include "function_pointers.h"
-
 /**
  * int_index - searches for an integar
  * @array: array of integers
@@ -13,16 +11,28 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i, res;
 
+	res = -1;
 	if (array && cmp)
 	{
+		if (size <= 0)
+		{
+			return (res);
+		}
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i]) != 0)
-				return (i);
+			cmp(array[i]);
+			if (cmp(array[i]) > 0)
+			{
+				res = i;
+				break;
+			}
+				if ((cmp(array[i]) == (-1)))
+				{
+					return (res);
+				}
 		}
 	}
-
-	return (-1);
+		return (res);
 }
