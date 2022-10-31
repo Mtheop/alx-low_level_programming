2f06130b52bf64dcb65918dfbff3ce02cdfcc1f7
Mtheop <motheophetla@gmail.com>
2022-10-31 08:11:43 -0700

    Reads text

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Readsa text file and prints POSIX stdout
+ * @filename: a pointer to file name
+ * @letters: the number of letters
+ *
+ * Return: 0 if fails or NULL
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,4 @@
+0x15-file_io
+
+how to find the right sources using file descriptors and when to use the 
+corrct file descriptors.
